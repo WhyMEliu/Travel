0eaf2fd7337759d9c1b342863204b07c82ea971e
peng <527453497@qq.com>
2018-08-19 22:12:01 +0800

    recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,78 @@
+<template>
+    <div>
+      <div class="title">热门推荐</div>
+      <ul>
+        <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+            <img class="item-img" :src="item.imgUrl"/>
+            <div class="item-into">
+              <p class="item-title">{{item.title}}</p>
+              <p class="item-desc">{{item.desc}}</p>
+              <button class="item-button">查看详情</button>
+            </div>
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeRecommend",
+        data(){
+          return {
+            recommendList:[{
+              id:"0001",
+              imgUrl:"http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg",
+              title:"上海迪士尼乐园",
+              desc:"中国内地首座迪士尼主题乐园"
+            },
+              {
+                id:"0001",
+                imgUrl:"http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg",
+                title:"上海迪士尼乐园",
+                desc:"中国内地首座迪士尼主题乐园"
+              },
+              {
+                id:"0001",
+                imgUrl:"http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg",
+                title:"上海迪士尼乐园",
+                desc:"中国内地首座迪士尼主题乐园"
+              }]
+          }
+        }
+    }
+</script>
+<style lang="stylus" scoped>
+  @import "~@/assets/styles/mixins.styl"
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    overflow: hidden
+    display : flex
+    height : 1.9rem
+    .item-img
+      width : 1.7rem
+      height : 1.7rem
+      padding : .1rem
+    .item-into
+      flex : 1
+      padding : .1rem
+      min-width : 0
+      .item-title
+        line-height : .54rem
+        font-size : .32rem
+        ellipsis()
+      .item-desc
+        line-height :.4rem
+        color : #ccc
+        ellipsis()
+      .item-button
+        line-height : .44rem
+        margin-top : .16rem
+        background : #ff9300
+        padding : 0 .2rem
+        border-radius : .06rem
+        color : #fff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl"/>
+        </div>
+        <div class="item-into">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "HomeWenkend",
+    data(){
+      return {
+        recommendList:[{
+          id:"0001",
+          imgUrl:"http://img1.qunarzz.com/sight/source/1505/9e/21df651e19af5d.jpg_r_640x214_3ea5bb38.jpg",
+          title:"寻找北京的皇城范儿",
+          desc:"数百年的宫廷庙宇，至今依旧威严霸气"
+        },
+          {
+            id:"0002",
+            imgUrl:"http://img1.qunarzz.com/sight/source/1505/9e/21df651e19af5d.jpg_r_640x214_3ea5bb38.jpg",
+            title:"寻找北京的皇城范儿",
+            desc:"数百年的宫廷庙宇，至今依旧威严霸气"
+          },
+          {
+            id:"0003",
+            imgUrl:"http://img1.qunarzz.com/sight/source/1505/9e/21df651e19af5d.jpg_r_640x214_3ea5bb38.jpg",
+            title:"寻找北京的皇城范儿",
+            desc:"数百年的宫廷庙宇，至今依旧威严霸气"
+          }]
+      }
+    }
+  }
+</script>
+<style lang="stylus" scoped>
+  @import "~@/assets/styles/mixins.styl"
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item-img-wrapper
+    overflow :hidden
+    height :0
+    padding-bottom : 33.4%
+  .item-img
+    width : 100%
+  .item-into
+    padding : .1rem
+    .item-title
+      line-height : .54rem
+      font-size : .32rem
+      ellipsis()
+    .item-desc
+      line-height :.4rem
+      color : #ccc
+      ellipsis()
+</style>
